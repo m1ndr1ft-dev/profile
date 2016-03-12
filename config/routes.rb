2fdd0379655088a49b2a960ac2b0891e98f1e91a
Andrey Arias <andreyarias35@gmail.com>
2016-03-11 19:24:51 -0800

    Removed sign_up functionallity

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
   devise_for :users
   resources :posts
   resources :projects
   resources :contacts, only: [:new, :create]
+
   get 'welcome/index'
   root 'welcome#index'
+
+  # Redirects to home_page
+  get '*path' => redirect('/')
+
 end
