2427743c7c10c8fbea4c72a1d2e5cff933b718df
Andrey Arias <andreyarias35@gmail.com>
2016-03-12 15:39:58 -0800

    Added static controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/resume'
+
   devise_for :users
-  resources :posts
-  resources :projects
-  resources :contacts, only: [:new, :create]
+  resources  :posts
+  resources  :projects
+  resources  :contacts, only: [:new, :create]
 
   get 'welcome/index'
   root 'welcome#index'
